38d6e20dd5d06bbdecfa1c4408c9f76b5bcf535f
Fida <khan.kansas@gmail.com>
2021-03-20 20:27:15 +0500

    Fixing Unused variable lint issue

--- src/redux/middlewares.js
+++ src/redux/middlewares.js
@@ -1,6 +1,3 @@
-import { SET_INVEST } from "utils/constants";
-import { getUpdatedLoan } from "utils/functions";
-
 /**
  * Applications Middlwares
  * @param {*} store
@@ -13,11 +10,5 @@ import { getUpdatedLoan } from "utils/functions";
  * @returns
  */
 export const handleInvest = (store) => (next) => (action) => {
-  if (false) {
-    const { data } = store.getState().app;
-    const { id, investAmount } = action.payload;
-    const existingLoan = data.find((loan) => loan.id === id);
-    action.payload = getUpdatedLoan(existingLoan, investAmount);
-  }
   next(action);
 };
